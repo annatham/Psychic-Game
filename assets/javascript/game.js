268fe4bf339b684940153d33826891bd4bce2bf5
Anna Thammakane <anna.thammakane@gmail.com>
2019-08-10 20:56:22 -0500

    some display showing on html

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,62 +2,87 @@
 var letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var wins = 0;
 var losses = 0;
-var guessesLeft = 5;
+var guessesLeft = 10;
 var userInput = [];
 
-
+//display on html
 var winsText = document.getElementById("wins");
 var lossesText = document.getElementById("losses");
 var guessesLeft = document.getElementById("guessesLeft");
 var userText = document.getElementById("userInput");
 
+//"computer" letter generator
+
+var randomLet = letters[Math.floor(Math.random() * letters.length)];
+console.log(randomLet.toLowerCase());
 
 //.onkeyup is input
 document.onkeyup = function (event) {
 
     var userInput = event.key;
 
-    var randomLet = "";
-    var randomLet = letters[Math.floor(Math.random() * letters.length)];
-    console.log(randomLet);
-
-    //randomly generate letter
-    
    if (userInput === randomLet) {
         wins++;
         guessesLeft = 5;
-        userInput = [];
+
+        //reset function
+        function reset () {
+            userInput.length = 0;
+        //computer chooses new letter
+            randomLet = letters[Math.floor(Math.random() * letters.length)];
+            console.log(randomLet.toLowerCase());        }
+        reset();
+    } 
+
+    else {
+        guessesLeft--;
     }
 
-    guesses();
+    if (guessesLeft === 0) {
+        losses++;
+        guessesLeft = 5;
+
+        function reset () {
+            userInput.length = 0;
+        //computer chooses new letter
+            randomLet = letters[Math.floor(Math.random() * letters.length)];
+            console.log(randomLet.toLowerCase());
+        }
+        reset();
+
+    }
+        
     if (userInput !== randomLet) {
         guessesLeft--;
     }
 
     if (guessesLeft == 0) {
         losses++;
-        userInput = [];
-        guessesLeft = 5;
+        guessesLeft =5;
     }
 //letter is only used once
     if (userInput.indexOf(userInput) >= 0) {
 
     } 
     
-    else {
 //user input is recorded
         userInput.push(userInput) = userInput;
-        console.log(userInput);
-    }
-// Display the user guesses and wins/losses.
+        innerHTML(userInput);
+    
+//Display the user guesses and wins/losses.
 
     // document.getElementById('wins') = wins;
     // document.getElementById('losses') = losses;
     // document.getElementById('guessesLeft') = guessesLeft;
 
 
-    winsText.textContent = "Wins: " + wins;
-    lossesText.textContent = "Losses: " + losses;
-    guessesLeft.textContent = "Guesses Left: " + ties;
-    userIput.textContent = "Your Guesses So Far: " + user;
+    winsText.innerHTML = "WINS: " + wins;
+    lossesText.innerHTML = "LOSSES: " + losses;
+    guessesLeft.innerHTML = "ATTEMPTS: " + guessesLeft;
+    userInput.innerHTML = "YOUR GUESSES SO FAR: " + userInput;
   }
+
+  //create reset function
+  //guesses return to 5
+  //empty userImput array
+  //computer will need to generate a new random letter
