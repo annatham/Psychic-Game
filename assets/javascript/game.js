ca5137afa3dbf28bb6651807f6bed5c381b7ea37
Anna Thammakane <anna.thammakane@gmail.com>
2019-08-10 21:47:29 -0500

    added button

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -11,43 +11,29 @@ var userInput = document.onkeyup;
 //"computer" letter generator
 
 var randomLet = letters[Math.floor(Math.random() * letters.length)];
-console.log(randomLet.toLowerCase());
+console.log(randomLet[0].toLowerCase());
 
 //display on html
 var winsText = document.getElementById("wins");
 var lossesText = document.getElementById("losses");
-var guessesLeft = document.getElementById("guessesLeft");
+var guessesText = document.getElementById("guessesLeft");
 var attemptsText = document.getElementById("attempts");
 var userText = document.getElementById("userInput");
 
 
 
-//reset function
-
-// function reset() {
-//     userInput.length = 0;
-// //computer chooses new letter
-//     randomLet = letters[Math.floor(Math.random() * letters.length)];
-//     console.log(randomLet.toLowerCase());        
-// }
 
 //.onkeyup is input
 document.onkeyup = function (event) {
 
     userInput = event.key;
     attempts.push(userInput);
-    // innerHTML(userInput);    
-
 
    if (userInput === randomLet) {
         wins++;
         guessesLeft = 5;
         attempts = [];
     } 
-
-    else {
-        guessesLeft--;
-    }
 
     if (guessesLeft === 0) {
         losses++;
@@ -61,9 +47,9 @@ document.onkeyup = function (event) {
     }
 
 //letter is only used once
-    if (userInput.indexOf(userInput) >= 0) {
+    // if (userInput.indexOf(userInput) >= 0) {
 
-    } 
+
     
 // //user input is recorded
 //         userInput.push(userInput) = userInput;
@@ -79,10 +65,30 @@ document.onkeyup = function (event) {
 
     winsText.innerHTML = "WINS: " + wins;
     lossesText.innerHTML = "LOSSES: " + losses;
-    guessesLeft.innerHTML = "GUESSES LEFT: " + guessesLeft;
+    guessesText.innerHTML = "GUESSES LEFT: " + guessesLeft;
     attemptsText.innerHTML = "ATTEMPTS: " + attempts;
     userText.innerHTML = "YOUR GUESSES SO FAR: " + userInput;
   }
+
+//reset function
+
+  function reset() {
+    var btn = document.createElement("BUTTON");
+    btn.innerHTML = "Reset Game";
+    document.body.appendChild(btn);
+    userInput.length = 0;
+
+  }
+    
+
+
+// function reset() {
+//     userInput.length = 0;
+// //computer chooses new letter
+//     randomLet = letters[Math.floor(Math.random() * letters.length)];
+//     console.log(randomLet.toLowerCase());        
+// }
+
 
   //create reset function
   //guesses return to 5
