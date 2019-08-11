ef01fcc4f2a6cf5de1135e318cb4d2909882a7d3
Anna Thammakane <anna.thammakane@gmail.com>
2019-08-10 22:55:14 -0500

    button not working

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,16 +2,13 @@
 var letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var wins = 0;
 var losses = 0;
-var guessesLeft = 10;
+var guessesLeft = 6;
 var attempts = [];
 
 //user input
 var userInput = document.onkeyup;
 
 //"computer" letter generator
-
-var randomLet = letters[Math.floor(Math.random() * letters.length)];
-console.log(randomLet[0].toLowerCase());
 
 //display on html
 var winsText = document.getElementById("wins");
@@ -21,17 +18,18 @@ var attemptsText = document.getElementById("attempts");
 var userText = document.getElementById("userInput");
 
 
-
-
 //.onkeyup is input
 document.onkeyup = function (event) {
 
     userInput = event.key;
     attempts.push(userInput);
 
+    var randomLet = letters[Math.floor(Math.random() * letters.length)];
+    console.log(randomLet.toLowerCase());
+
    if (userInput === randomLet) {
         wins++;
-        guessesLeft = 5;
+        guessesLeft = 6;
         attempts = [];
     } 
         
@@ -41,7 +39,7 @@ document.onkeyup = function (event) {
 
     if (guessesLeft === 0) {
         losses++;
-        guessesLeft = 5;
+        guessesLeft = 6;
         attempts = [];
     }
 
@@ -53,14 +51,6 @@ document.onkeyup = function (event) {
 // //user input is recorded
 //         userInput.push(userInput) = userInput;
 //         innerHTML(userInput);
-    
-//Display the user guesses and wins/losses.
-
-    // document.getElementById("wins") = wins;
-    // document.getElementById("losses") = losses;
-    // document.getElementById("guessesLeft") = guessesLeft;
-    // document.getElementById("userInput") = userInput;
-
 
     winsText.innerHTML = "WINS: " + wins;
     lossesText.innerHTML = "LOSSES: " + losses;
@@ -72,21 +62,13 @@ document.onkeyup = function (event) {
 //reset function
 
   function myFunction() {
-    document.getElementById("wins", "losses", "guessesLeft","attempts", "userInput").reset();
-
+    document.getElementById("myForm").reset();
   }
     
 
 
 // function reset() {
 //     userInput.length = 0;
-// //computer chooses new letter
-//     randomLet = letters[Math.floor(Math.random() * letters.length)];
-//     console.log(randomLet.toLowerCase());        
+       
 // }
 
-
-  //create reset function
-  //guesses return to 5
-  //empty userImput array
-  //computer will need to generate a new random letter
